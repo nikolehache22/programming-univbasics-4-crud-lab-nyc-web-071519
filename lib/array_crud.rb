be03e4e313cc03eae18c1c5db071916f680cdea6
Nikole Hache <nh131215@gmail.com>
2019-07-09 14:01:14 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,7 +8,7 @@ end
 
 def add_element_to_end_of_array(array, element)
   ["red", "blue"]
-  << "yellow"
+  add_element_to_end_of_array << "yellow"
 end
 
 def add_element_to_start_of_array(array, element)
